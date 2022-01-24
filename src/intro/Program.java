ff2638df6c81f78c0b2058b85f716b29f7b8d7ba
Märt Kalmo <mart.kalmo@gmail.com>
2022-01-24 15:28:43 +0200

    updated for 2022

---
+++ src/intro/Program.java
@@ -0,0 +1,25 @@
+package intro;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        int decimal = asDecimal("11001101");
+
+        System.out.println(decimal); // 205
+    }
+
+    private static String asString(int input) {
+        return "";
+    }
+
+    private static int asDecimal(String input) {
+        return 0;
+    }
+
+    private static int pow(int arg, int power) {
+        // Java has Math.pow() but this time write your own implementation.
+
+        return 0;
+    }
+}
