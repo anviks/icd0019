f182a5c8ac2bb80454f277bf96e2f7f3b2b75e05
anviks <anviks@ttu.ee>
2023-01-30 21:00:54 +0200

    ex01

--- src/intro/Program.java
+++ src/intro/Program.java
@@ -11,6 +11,7 @@ public class Program {
         System.out.println(pow(2, 5));
         System.out.println(pow(4, 2));
         System.out.println(pow(4, 0));
+
     }
 
     public static String asString(int input) {
