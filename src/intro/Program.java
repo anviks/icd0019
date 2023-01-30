fa11b3f36d9c8801bf260d089facd20e0e58f041
anviks <anviks@ttu.ee>
2023-01-30 19:55:25 +0200

    EX01

--- src/intro/Program.java
+++ src/intro/Program.java
@@ -7,19 +7,30 @@ public class Program {
         int decimal = asDecimal("11001101");
 
         System.out.println(decimal); // 205
+        System.out.println(asString(205));
+        System.out.println(pow(2, 5));
+        System.out.println(pow(4, 2));
+        System.out.println(pow(4, 0));
     }
 
     public static String asString(int input) {
-        return "";
+        return Integer.toBinaryString(input);
     }
 
     public static int asDecimal(String input) {
-        return 0;
+        return Integer.parseInt(input, 2);
     }
 
     private static int pow(int arg, int power) {
         // Java has Math.pow() but this time write your own implementation.
 
-        return 0;
+        int result = 1;
+
+        for (int i = 0; i < power; i++) {
+            result *= arg;
+        }
+
+        return result;
     }
+
 }
