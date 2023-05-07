e1a910568d6684346aa6cda24775d75f7fa84299
anviks <anviks@ttu.ee>
2023-05-07 18:12:53 +0300

    Initial commit

--- src/reflection/tester/MyTest.java
+++ src/reflection/tester/MyTest.java
@@ -1,5 +1,9 @@
 package reflection.tester;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface MyTest {
     Class<? extends Throwable> expected() default None.class;
 
--- src/reflection/tester/TestRunner.java
+++ src/reflection/tester/TestRunner.java
@@ -1,13 +1,51 @@
 package reflection.tester;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TestRunner {
 
+    private final List<String> results = new ArrayList<>();
+
     public void runTests(List<String> testClassNames) {
+        for (String testClass : testClassNames) {
+            try {
+                Class<?> c = Class.forName(testClass);
+                Object instance = c.getConstructor().newInstance();
+                Arrays.stream(c.getDeclaredMethods()).forEach(method -> {
+                    if (method.getAnnotation(MyTest.class) != null) {
+                        runTest(method, instance);
+                    }
+                });
+            } catch (ClassNotFoundException | InvocationTargetException | InstantiationException |
+                     IllegalAccessException | NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void runTest(Method method, Object instance) {
+        String result = method.getName() + "() - ";
+        MyTest annotation = method.getAnnotation(MyTest.class);
+
+        try {
+            method.invoke(instance);
+            result += annotation.expected() == MyTest.None.class ? "OK" : "FAILED";
+        } catch (InvocationTargetException e) {
+            Class<? extends Throwable> expected = annotation.expected();
+            result += expected.isAssignableFrom(e.getCause().getClass()) ? "OK" : "FAILED";
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        results.add(result);
     }
 
     public String getResult() {
-        return null;
+        return String.join("\n", results);
     }
 }
